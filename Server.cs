2d0131ea149874014330381488d60dd976f57e14
whowhereq <kirwhow@gmail.com>
2023-06-21 11:47:41 +0300

    описали метод "Start" для класса "Server".

--- Server.cs
+++ Server.cs
@@ -1,3 +1,13 @@
-﻿internal class Server
+﻿using CarDealerApp.Server;
+using System.Net.Sockets;
+
+internal class Server
 {
+    private List<Car> cars;
+    private Socket serverSocket;
+
+    public Server()
+    {
+        cars = new List<Car>();
+    }
 }
