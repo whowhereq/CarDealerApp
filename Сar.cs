280e36eb442e342c75065dc76b0b653361b65022
whowhereq <kirwhow@gmail.com>
2023-06-21 11:25:30 +0300

    Добавили и описали класс Car.

---
+++ Сar.cs
@@ -0,0 +1,10 @@
+﻿namespace CarDealerApp.Server
+{
+    internal class Сar
+    {
+        public string Brand { get; set; }
+        public ushort Year { get; set; }
+        public float EngineVolume { get; set; }
+        public byte? NumberOfDoors { get; set; }
+    }
+}
