b3a5b4edf3977f453e0f811ac4880b6a64b892db
whowhereq <kirwhow@gmail.com>
2023-06-23 16:16:14 +0300

    Update ClientMain.cs

--- CarDealerApp.Client/ClientMain.cs
+++ CarDealerApp.Client/ClientMain.cs
@@ -5,6 +5,25 @@ public class ClientMain
     public static void Main(string[] args)
     {
         Client client = new Client();
-        client.ConnectToServer();
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("Input 1 for connect to server");
+                string? choose = Console.ReadLine();
+                if (choose == "1")
+                {
+                    client.ConnectToServer();
+                }
+                else
+                {
+                    Console.WriteLine("Erorr: not correct inpud");
+                }
+            }
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.ToString());
+        }
     }
 }
